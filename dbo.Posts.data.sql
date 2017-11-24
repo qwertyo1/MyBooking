2b9d8d224fa0900c395c42b42b73d7f857702b1c
Владимир <klipert1968@yandex.ru>
2017-11-24 14:00:57 +0600

    Deployed

--- dbo.Posts.data.sql
+++ dbo.Posts.data.sql
@@ -1,3 +1,26 @@
 ﻿SET IDENTITY_INSERT [dbo].[Posts] ON
+INSERT INTO [dbo].[Posts] ([Id], [Name], [Content], [Price], [UserName], [CreationDate], [Image]) VALUES (5, N'Beautiful house at the beach', N'Romantic Private Apt - 17M to Beach offers pet-friendly accommodations in Greenwood, just 49.9 km from Rehoboth Beach. The property features views of the garden and is 35.4 km from Dover. Free WiFi is featured and free private parking is available on site.
 
+The kitchenette is fitted with a dishwasher and an oven. A flat-screen TV with satellite channels is available.
+
+Lewes is 43.5 km from Romantic Private Apt - 17M to Beach, and Cambridge is 49.9 km from the property. Salisbury-Ocean City Wicomico Regional Airport is 56.3 km away. 
+
+We speak your language!', 110, N'Vova', N'2017-11-24 02:28:31', <Binary Data>)
+INSERT INTO [dbo].[Posts] ([Id], [Name], [Content], [Price], [UserName], [CreationDate], [Image]) VALUES (6, N'Barn & Hay Loft Retreat', N'Barn & Hay Loft Retreat is a holiday home set in Greenwood. Free WiFi is available .
+
+The dry kitchen comes with an oven. There is a bathroom in the Main House.
+
+The home offers views of the woodlands and a farm.
+
+A selection of activities are offered in the area, such as golfing, fishing and hiking.', 210, N'Vova', N'2017-11-24 02:29:45', <Binary Data>)
+INSERT INTO [dbo].[Posts] ([Id], [Name], [Content], [Price], [UserName], [CreationDate], [Image]) VALUES (7, N'Sealark Apartment ', N'Located 400 m from the beach, Sealark Apartment is set in Avalon. Guests staying at this apartment have access to a fully equipped kitchen.
+
+The apartment also provides guests with a bathroom.
+
+The property is located near many shops and restaurants. Atlantic City is 40 km from the apartment, while Cape May is 24 km away. The nearest airport is Atlantic City International Airport, 42 km from the property. ', 130, N'Vova', N'2017-11-24 02:30:50', <Binary Data>)
+INSERT INTO [dbo].[Posts] ([Id], [Name], [Content], [Price], [UserName], [CreationDate], [Image]) VALUES (8, N'Beachcomber Resort', N'Located in Avalon, Beachcomber Resort offers a garden and outdoor pool. Atlantic City is 43.5 km from the property.
+
+All units are air conditioned and include a cable flat-screen TV. Some units have a sitting and/or dining area. There is also a kitchen, fitted with a dishwasher and oven. There is a private bathroom with a bathtub or shower in each unit. Towels and bed linen are provided.
+
+Beachcomber Resort also includes a hot tub.', 170, N'Vova', N'2017-11-24 02:31:44', <Binary Data>)
 SET IDENTITY_INSERT [dbo].[Posts] OFF

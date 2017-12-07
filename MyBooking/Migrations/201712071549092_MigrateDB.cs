c8abe0309a304051c79f0b02d950050567a509ae
Владимир <klipert1968@yandex.ru>
2017-12-07 22:09:58 +0600

    Database migrations added

---
+++ MyBooking/Migrations/201712071549092_MigrateDB.cs
@@ -0,0 +1,31 @@
+namespace MyBooking.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MigrateDB : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Posts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Content = c.String(nullable: false),
+                        Price = c.Int(nullable: false),
+                        UserName = c.String(),
+                        CreationDate = c.DateTime(nullable: false),
+                        Image = c.Binary(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Posts");
+        }
+    }
+}
---
+++ MyBooking/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace MyBooking.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MyBooking.Models.PostContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MyBooking.Models.PostContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
